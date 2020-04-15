c00f0dcf3c49e6871b949f907ed9e56afc673728
Екатерина Чиркова <enchirkova@edu.hse.ru>
2020-04-16 01:47:50 +0300

    Task04 - first version

---
+++ Iterators/Task04/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = int.Parse(Console.ReadLine());
+            MyInts myInts = new MyInts();
+            IEnumerator enumerator = myInts.MyEnumerator(value);
+
+            while (enumerator.MoveNext())
+            {
+                Console.Write(enumerator.Current + " ");
+            }
+            Console.WriteLine();
+            while (enumerator.MoveNext())
+            {
+                Console.Write(enumerator.Current + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class MyInts : IEnumerator
+    {
+        int index = 0;
+        int N;
+        
+        public IEnumerator MyEnumerator(int N)
+        {
+            this.N = N;
+            return this;
+        }
+        
+        public bool MoveNext()
+        {
+            if (index == N)
+            {
+                Reset();
+                return false;
+            }
+
+            index++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            index = 0;
+        }
+
+        public object Current
+        {
+            get {
+                return index * index;
+            }
+        }
+    }
+}
