7f663063630deb3cd7dc5139fd09ca6a57767bbe
Екатерина Чиркова <enchirkova@edu.hse.ru>
2020-04-16 01:25:01 +0300

    Task01 and Task03 are completed

--- Iterators/Task01/Program.cs
+++ Iterators/Task01/Program.cs
@@ -3,13 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Необходимо построить ряд чисел Фибоначчи, ограниченный числом, введенным с клавиатуры.
+ * 
+ * Пример входных данных:
+ * 6
+ * Пример выходных данных:
+ * 1 1 2 3 5
+ * Пояснение:
+ * следующее число 3 + 5 = 8 не выводится на экран, так как 8 > 6.
+ * 
+ * В случае ввода некорректных данных выбрасывайте ArgumentException.
+ * 
+*/
 namespace Task01
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int value = // int.Parse(Console.ReadLine());
+                foreach (int el in Fibonacci(value))
+                {
+                    Console.Write(el + " ");
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.Write("error");
+            }
+            //Console.ReadKey();
+        }
+
+        public static IEnumerable<int> Fibonacci(int maxValue)
+        {
+            //yield return 1;
+            //yield return 1;
+
+            //var last = 1;
+            //var beforeLast = 1;
+
+            //while (last + beforeLast <= maxValue)
+            //{
+            //    var current = last + beforeLast;
+            //    beforeLast = last;
+            //    last = current;
+
+            //    yield return current;
+            //}
         }
     }
 }
---
+++ Iterators/Task03/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+
+/* На вход подается число N.
+ * На каждой из следующих N строках записаны имя и фамилия человека, 
+ * разделенныые одним пробелом.
+ * Используя собственноручно написанный итератор, выведите имена людей,
+ * отсортированные в лексико-графическом порядке в формате 
+ *      <Фамилия_с_большой_буквы> <Заглавная_первая_буква_имени>.
+ * Затем выведите имена людей в исходном порядке.
+ * 
+ * Код, данный в условии, НЕЛЬЗЯ стирать, его можно только дополнять.
+ * 
+ * Пример входных данных:
+ * 3
+ * Bill Banana
+ * Alex Apple
+ * Clark Carrot
+ * 
+ * Пример выходных данных:
+ * Apple A.
+ * Banana B.
+ * Carrot C.
+ * 
+ * Banana B.
+ * Apple A.
+ * Carrot C.
+ * 
+ * В случае ввода некорректных данных выбрасывайте ArgumentException.
+*/
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int N = // int.Parse(Console.ReadLine());
+                Person[] people = new Person[N];
+            }
+            catch (ArgumentException)
+            {
+                Console.Write("error");
+            }
+            //for (int i = 0; i < N; i++)
+            //{
+            //    string[] arr = Console.ReadLine().Split();
+            //    people[i] = new Person(arr[0], arr[1]);
+            //}
+            
+            People peopleList = new People(people);
+            
+            foreach (Person p in peopleList)
+                Console.WriteLine(p);
+
+            foreach (Person p in peopleList.People)
+                Console.WriteLine(p);
+
+            //Console.Read();
+        }
+    }
+
+    public class Person//: IComparable<Person>
+    {
+        public string firstName;
+        public string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        //public int CompareTo(Person other)
+        //{
+        //    return this.ToString().CompareTo(other.ToString());
+        //}
+
+        //public override string ToString()
+        //{
+        //    return lastName + " " + firstName[0] + ".";
+        //}
+    }
+
+
+    public class People : IEnumerable
+    {
+        private Person[] _people;
+        public Person[] People
+        {
+            get {
+                return _people;
+            }
+        }
+        //public People(Person[] pArray)
+        //{
+        //    _people = new Person[pArray.Length];
+
+        //    for (int i = 0; i < pArray.Length; i++)
+        //    {
+        //        _people[i] = pArray[i];
+        //    }
+        //}
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public PeopleEnum GetEnumerator()
+        {
+            return new PeopleEnum(_people);
+        }
+    }
+    
+    public class PeopleEnum : IEnumerator
+    {
+        public Person[] _people;
+
+        //int position = -1;
+
+        //public PeopleEnum(Person[] list)
+        //{
+        //    _people = (Person[])list.Clone();
+        //    Array.Sort(_people);
+        //}
+
+        public bool MoveNext()
+        {
+            //position++;
+            //return (position < _people.Length);
+        }
+
+        public void Reset()
+        {
+            //position = -1;
+        }
+
+        //object IEnumerator.Current
+        //{
+        //    get {
+        //        return Current;
+        //    }
+        //}
+
+        public Person Current
+        {
+            //get {
+            //    try
+            //    {
+            //        return _people[position];
+            //    }
+            //    catch (IndexOutOfRangeException)
+            //    {
+            //        throw new InvalidOperationException();
+            //    }
+            //}
+        }
+    }
+}
