4b4603a6f2372f19fdef189ef8938ba39842ee54
Екатерина Чиркова <enchirkova@edu.hse.ru>
2020-04-16 15:46:24 +0300

    Task03 and Task04 are completed

--- Iterators/Task04/Program.cs
+++ Iterators/Task04/Program.cs
@@ -1,65 +1,89 @@
 ﻿using System;
 using System.Collections;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/* На вход подается число N.
+ * Нужно создать коллекцию из N квадратов последовательного ряда натуральных чисел 
+ * и вывести ее на экран дважды. Элементы коллекции разделять пробелом. 
+ * Выводы всей коллекции разделять переходом на новую строку.
+ * Не хранить всю коллекцию в памяти.
+ * 
+ * Код, данный в условии, НЕЛЬЗЯ стирать, его можно только дополнять.
+ * Не использовать yield и foreach. Не вызывать метод Reset() в классе Program.
+ * 
+ * Пример входных данных:
+ * 3
+ * 
+ * Пример выходных данных:
+ * 1 4 9
+ * 1 4 9
+ * 
+ * В случае ввода некорректных данных выбрасывайте ArgumentException.
+*/
 namespace Task04
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int value = int.Parse(Console.ReadLine());
-            MyInts myInts = new MyInts();
+            try
+            {
+                int value = // int.Parse(Console.ReadLine());
+                MyInts myInts = new MyInts();
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("error");
+            }
             IEnumerator enumerator = myInts.MyEnumerator(value);
 
-            while (enumerator.MoveNext())
-            {
-                Console.Write(enumerator.Current + " ");
-            }
+            IterateThroughEnumeratorWithoutUsingForeach(enumerator);
             Console.WriteLine();
-            while (enumerator.MoveNext())
-            {
-                Console.Write(enumerator.Current + " ");
-            }
-            Console.ReadKey();
+            IterateThroughEnumeratorWithoutUsingForeach(enumerator);
+            //Console.ReadKey();
+        }
+
+        static void IterateThroughEnumeratorWithoutUsingForeach(IEnumerator enumerator)
+        {
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.Write(enumerator.Current + " ");
+            //}
         }
     }
 
-    class MyInts : IEnumerator
+    class MyInts : IEnumerator // НЕ МЕНЯТЬ ЭТУ СТРОКУ
     {
-        int index = 0;
-        int N;
+        //int index = 0;
+        //int N;
         
-        public IEnumerator MyEnumerator(int N)
-        {
-            this.N = N;
-            return this;
-        }
+        //public IEnumerator MyEnumerator(int N)
+        //{
+            //this.N = N;
+            //return this;
+        //}
         
         public bool MoveNext()
         {
-            if (index == N)
-            {
-                Reset();
-                return false;
-            }
+            //if (index == N)
+            //{
+            //    Reset();
+            //    return false;
+            //}
 
-            index++;
-            return true;
+            //index++;
+            //return true;
         }
 
-        public void Reset()
-        {
-            index = 0;
-        }
+        //public void Reset()
+        //{
+        //    index = 0;
+        //}
 
         public object Current
         {
-            get {
-                return index * index;
-            }
+            //get {
+            //    return index * index;
+            //}
         }
     }
 }
